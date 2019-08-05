2fd0c91e35767cbd827f2a8e9cdb7780a5a90033
Chai Chai <c.chai@hotmail.co.uk>
2019-08-05 13:58:45 +0100

    styling

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -4,7 +4,7 @@ $(function() {
         room_id = $element.data('room-id')
         messageTemplate = $('[data-role="message-template"]')[0];
 
-    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)
+    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 0)
 
     App.cable.subscriptions.create(
       {
@@ -19,9 +19,16 @@ $(function() {
           content.find('[data-role="message-text"]').text(data.message);
           content.find('[data-role="message-date"]').text(data.updated_at);
           $element.append(content);
-          $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
+          $element.animate({ scrollTop: $element.prop("scrollHeight")}, 500);
         }
       }
     );
   });
 });
+
+  function myFunction() {
+  var elmnt = document.getElementById("myDIV");
+  var x = elmnt.scrollLeft;
+  var y = elmnt.scrollTop;
+  document.getElementById ("demo").innerHTML = "Horizontally: " + x + "px<br>Vertically: " + y + "px";
+}
