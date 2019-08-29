b6c70e3a9a3df4ce26faa3d70545e773c8629e12
Chai Chai <c.chai@hotmail.co.uk>
2019-08-29 14:46:06 +0100

    strong params for messages

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -2,12 +2,13 @@ class RoomMessagesController < ApplicationController
   before_action :load_new_message
 
   def create
-    @room_message = RoomMessage.create  user: current_user,
-                                        room: @room,
-                                        message: params.dig(:room_message, :message)
+    # @room_message = RoomMessage.create  user: current_user,
+    #                                     room: @room,
+    #                                     message: params.dig(:room_message, :message)
+
+    @room_message = RoomMessage.create(room_message_params)
 
     authorize @room_message
-
     RoomChannel.broadcast_to @room, @room_message
   end
 
@@ -15,5 +16,9 @@ class RoomMessagesController < ApplicationController
 
   def load_new_message
     @room = Room.find params.dig(:room_message, :room_id)
+  end
+
+  def room_message_params
+    params.require(:room_message).permit(:message, :user_id, :room_id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  validates :name, presence: true
+
   has_many :room_messages, dependent: :destroy, inverse_of: :room
   has_and_belongs_to_many :users
 end
